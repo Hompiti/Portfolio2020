70e8b5242c486271d52bd26f107623af7c68572b
hompiti <hompiti@gmail.com>
2021-01-12 13:58:29 +0000

    javascript

--- scripts.js
+++ scripts.js
@@ -1,1 +1,19 @@
-// hello world
+    var navbar = document.querySelector(".navbar")
+    var ham = document.querySelector(".ham")
+
+
+  function toggleHamburger(){
+    navbar.classList.toggle("showNav")
+    ham.classList.toggle("showClose")
+}
+
+  ham.addEventListener("click", toggleHamburger)
+
+// toggle when clicking on links
+    var menuLinks = document.querySelectorAll(".menuLink")
+    menuLinks.forEach(
+
+    function(menuLink) {
+    menuLink.addEventListener("click", toggleHamburger)
+  }
+)
